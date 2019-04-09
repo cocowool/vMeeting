6d7749b2fe0dcae434ea02984240cfd0c4c14ba7
Green Wang <cocowool@qq.com>
2019-04-09 18:57:30 +0800

    fix vue-loader problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     mode: "development",
     entry: './src/index.js',
+    module: {
+        rules: [
+            { test: /\.vue$/, loader: 'vue-loader', exclude: '/node_modules/'}
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'vMeeting Project',
             template: './src/index.html'
-        })
+        }),
+        new VueLoaderPlugin()
+        // new webpack.LoaderOptionPlugin({
+        //     test: /\.vue$/,
+        //     options: [
+        //         rules : {use:'vue-loader'}
+        //     ]
+        // })
     ],
     output: {
         filename: 'main.js',
