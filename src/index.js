dd7d5aa293df163463efc3178d5e793619478208
Green Wang <cocowool@qq.com>
2019-04-11 19:32:23 +0800

    learn module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Main from './components/Main.vue'
+var m = require('./meeting')
 
 console.log("Hello webpack")
+m.sayHello();
 
 const app = new Vue({
     el: '#app',
---
+++ src/meeting.js
@@ -0,0 +1,14 @@
+var name;
+
+exports.sayHello = function(){
+    console.log('Hello from module');
+}
+
+// export default {
+//     setName () {
+//         name = thyName;
+//     },
+//     sayHello (){
+//         console.log('Hello from module');
+//     }
+// };
