8f9a71dfe81b030130a57a80a5ff64141b7e5a55
Green Wang <cocowool@qq.com>
2019-05-13 19:34:01 +0800

    modify transition

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,7 +4,9 @@
             <router-link to="/">Home</router-link>
             <router-link to="/cal">Calendar</router-link>
             <router-link to="/about">About</router-link>
-            <router-view class="view"></router-view>
+            <transition name="fade" mode="out-in">
+                <router-view class="view"></router-view>
+            </transition>
         </div>
     </div>
 </template>
