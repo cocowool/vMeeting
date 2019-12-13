a9e8b813409c4c66da8c5e56235022b8245ca20b
Green Wang <cocowool@qq.com>
2019-12-13 20:50:33 +0800

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const ExtractTextPlugin = rquire('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     mode: "development",
