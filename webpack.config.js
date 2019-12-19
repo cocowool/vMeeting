a8206967a1c3f2a0693e2d210b9fd512658eb8cb
Green Wang <cocowool@gmail.com>
2019-12-19 12:05:33 +0800

    import minicss plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+//const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: "development",
@@ -11,7 +12,12 @@ module.exports = {
             { test: /\.vue$/, loader: 'vue-loader', exclude: '/node_modules/'},
             { test: /\.css$/, use: [ 'style-loader', 'css-loader'] },
             //{ test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] } //这个方式将css打包进了js文件
-            { test: /\.scss$/, use: ExtractTextPlugin.extract({use:['css-loader', 'sass-loader'], fallback: 'style-loader'})}
+            { test: /\.scss$/, use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: '../dist'
+                }
+            }, 'style-loader', 'css-loader', 'sass-loader']}
         ]
     },
     plugins: [
@@ -19,6 +25,11 @@ module.exports = {
             title: 'vMeeting Project',
             template: './src/index.html'
         }),
+	new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+        ignoreOrder: false
+	}),
         new VueLoaderPlugin()
         // new webpack.LoaderOptionPlugin({
         //     test: /\.vue$/,
